a194048cf9f6715a1c6c07bb3042fb388f06368a
Camilo Romero <ca.romerom1@uniandes.edu.co>
2024-03-13 18:15:12 -0500

    Include confirmation screen

---
+++ app/register/confirmation.tsx
@@ -0,0 +1,32 @@
+import { CheckMark, View } from "@/components";
+import { Image, StyleSheet } from "react-native";
+
+export default function RegisterConfirmationPage() {
+  return (
+    <View style={styles.page}>
+      <Image
+        style={styles.background}
+        source={require("@/assets/images/background.png")}
+      />
+      <View style={styles.checkmarkContainer}>
+        <CheckMark />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+  },
+  checkmarkContainer: {
+    transform: [{ translateX: 12 }],
+  },
+});
